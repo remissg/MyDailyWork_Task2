09dd865e784609a00b17f4e563a950256f50242c
Joydip Maiti <maitijoydip888@gmail.com>
2025-11-27 19:49:04 +0530

    server.js updated

---
+++ server.js
@@ -0,0 +1,42 @@
+// Pakages imports
+import express from 'express';
+import expressAsyncErrors from 'express-async-errors';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import cors from 'cors';
+import morgan from 'morgan';
+// Files imports
+import connectDB from './config/db.js';
+// routes imports
+import testRoutes from './routes/testRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import errorMiddleware from './middlewares/errorMiddleware.js';
+
+// config dotenv
+dotenv.config();
+
+// mongoDB connection
+connectDB();
+
+// rest object
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// routes
+app.use('/api/v1/test', testRoutes);
+app.use('/api/v1/auth', authRoutes);
+
+// Validation Middleware
+app.use(errorMiddleware);
+
+// port
+const PORT = process.env.PORT || 8080;
+
+// listen
+app.listen(PORT, () => {
+  console.log(`Node Server Running In ${process.env.DEV_MODE} Mode On Port no ${PORT}`.bgCyan.white);
+});
