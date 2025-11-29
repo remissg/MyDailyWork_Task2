42a80d7c887c82bf4106d1188e525a97bd284cc0
Joydip Maiti <maitijoydip888@gmail.com>
2025-11-29 20:14:49 +0530

    routes\testRoutes.js updated

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { testPostController } from '../controllers/testController.js';
+import userAuth from '../middlewares/authMiddleware.js';
 
 // router object
 const router = express.Router();
 
 // test route
-router.post('/test-post', testPostController)
+router.post('/test-post', userAuth, testPostController)
 
 // export router
 export default router;
