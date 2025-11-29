1a0eec1cfd474d775aec98e0189fe50a858d3a58
Joydip Maiti <maitijoydip888@gmail.com>
2025-11-29 20:14:37 +0530

    routes\jobsRoutes.js created

---
+++ routes/jobsRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import userAuth from './../middlewares/authMiddleware.js';
+import { createJobController, deleteJobController, getAllJobsController, jobStatsController, updateJobController } from '../controllers/jobsController.js';
+
+const router = express.Router();
+
+// routes
+// CREATE JOB || POST
+router.post('/create-job', userAuth, createJobController)
+
+// GET JOBS || GET
+router.get('/get-job', userAuth, getAllJobsController)
+
+// UPDATE JOB || PATCH
+router.patch('/update-job/:id', userAuth, updateJobController)
+
+//  DELETE JOB || DELETE
+router.delete("/delete-job/:id", userAuth, deleteJobController)
+
+// JOBS STATS FILTER || GET
+router.get("/job-stats", userAuth, jobStatsController)
+
+export default router;
