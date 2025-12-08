10d9a693674107962f0b720b7328fa41515995aa
Joydip Maiti <maitijoydip888@gmail.com>
2025-12-08 14:53:46 +0530

    feat: Add `node_modules`, `.env`, `.DS_Store`, and `uploads/` to `.gitignore`.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MyDailyWork_Task2/',
   server: {
     proxy: {
       '/api': {
